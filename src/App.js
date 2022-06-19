4cd48dde4032a05d46351b7bcadfd9ffaadbafd9
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-06-19 14:28:23 -0400

    page links work

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
@@ -9,25 +11,22 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 
 function App() {
-  const [contactSelected, setContactSelected] = useState(false);
+  // const [contactSelected, setContactSelected] = useState(false);
 
   return (
-    <div className="App">
-      <Nav setContactSelected={setContactSelected}></Nav>
-      <main>
-        {!contactSelected ? (
-          <Hero></Hero>
-        ) : (
-          <>
-            <Resume></Resume>
-            <Portfolio></Portfolio>
-            <About></About>
-            {/* <Contact></Contact> */}
-          </>
-        )}
-      </main>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,55 +1,32 @@
 import React from "react";
-// import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function Nav(props) {
-  const { contactSelected, setContactSelected } = props;
+function Nav() {
+  //   const { contactSelected, setContactSelected } = props;
 
   return (
     <nav>
       <h1>Karly Weatherstein</h1>
       <ul>
-        {/* Links to routes */}
-        {/* <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link> */}
         {/* Home */}
         <li>
-          <a href="#about" onClick={() => setContactSelected(false)}>
-            Home
-          </a>
+          <Link to="/">Home</Link>
         </li>
         {/* About me */}
-        <li className={` ${contactSelected && "navActive"}`}>
-          <span onClick={() => setContactSelected(true)}>About Me</span>
+        <li>
+          <Link to="/about">About Me</Link>
         </li>
         {/* Portfolio */}
-        <li className={`${contactSelected && "navActive"}`}>
-          <span
-            onClick={() => {
-              setContactSelected(false);
-            }}
-          >
-            Portfolio
-          </span>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
         </li>
         {/* Contact */}
-        <li className={`${contactSelected && "navActive"}`}>
-          <span
-            onClick={() => {
-              setContactSelected(false);
-            }}
-          >
-            Contact
-          </span>
+        <li>
+          <Link to="/contact">Contact</Link>
         </li>
         {/* Resume */}
-        <li className={` ${contactSelected && "navActive"}`}>
-          <span
-            onClick={() => {
-              setContactSelected(false);
-            }}
-          >
-            Resume
-          </span>
+        <li>
+          <Link to="/resume">Resume</Link>
         </li>
       </ul>
     </nav>
